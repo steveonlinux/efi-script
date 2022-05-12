c84d821eee2a4ca70913deaa3b6897bca839ce6c
Stephen Marks <stephenmarks5309@gmail.com>
2022-05-12 01:40:01 -0700

    Corrected rootID; edited outputs

--- efi_boot.sh
+++ efi_boot.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-efibootmgr -b 0 -B
-efibootmgr --disk /dev/nvme0n1 --part 1 --create --label "Arch Linux" --loader /vmlinuz-linux --unicode 'root=PARTUUID=e7529a94-a769-4efd-91f2-2e7d28ede9cb rw initrd=\initramfs-linux.img rd.driver.blacklist=nouveau nvidia-drm.modeset=1' --verbose --timeout 0
+efibootmgr -b 0 -B > /dev/null
+printf "Deleted boot entry: 0000\n\n"
+printf "Reloaded entry 0000...\n"
+efibootmgr --disk /dev/nvme0n1 --part 1 --create --label "Arch Linux" --loader /vmlinuz-linux --unicode 'root=PARTUUID=fd8086ba-4136-bd4e-87d2-9171ba8c53c9 rw initrd=\initramfs-linux.img rd.driver.blacklist=nouveau nvidia-drm.modeset=1' --verbose --timeout 0 | grep "Boot0000"
